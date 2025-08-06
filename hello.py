8ded9de673921f7a106b4181602f29ce00fd356d
Tanatap Treerapragon <thanathep.ice@gmail.com>
2025-08-06 11:04:01 +0700

    feat: add current date to greeting

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
-import config
+import datetime 
+
+class config:
+    APP_NAME = "MyApp"
+
 def say_hello(name):
     print(f"Hello, {name}! Welcome to {config.APP_NAME}!")
+
+def sqy_hello(name):
+    now = datetime.datetime.now()
+    print(f"Hello,{name} from {config.APP_NAME}!")
+    print(f"Today is {now.strftime('%Y-%m-%d')}")
 
 def greet_user():
     user_name = input("Please enter your name: ")
